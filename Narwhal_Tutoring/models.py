cad5712df9e1c3da4c25cf1c1dc80e8336210660
NoahTunesLaunchpadYT <noahswest@icloud.com>
2024-01-21 06:09:21 +1100

    Featured Tutors

--- Narwhal_Tutoring/models.py
+++ Narwhal_Tutoring/models.py
@@ -9,10 +9,11 @@ class User(AbstractUser):
     tutor = models.BooleanField(default=False)
 
     # Tutors Only
-    suburb = models.CharField(max_length=100, blank=True)
-    subjects = models.ManyToManyField(Subject, related_name="tutors", blank=True)
-    atar = models.DecimalField(max_digits=4, decimal_places=2, blank=True)
-    available = models.BooleanField(default=False, blank=True)
+    suburb = models.CharField(max_length=100, null=True)
+    subjects = models.ManyToManyField(Subject, related_name="tutors", null=True)
+    atar = models.DecimalField(max_digits=4, decimal_places=2, null=True)
+    available = models.BooleanField(default=False, null=True)
+    pfp_url = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return f"{self.username}"
--- Narwhal_Tutoring/views.py
+++ Narwhal_Tutoring/views.py
@@ -5,10 +5,32 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.http import JsonResponse
 from .models import *
+from django.conf import settings
+import os
+from django.contrib.staticfiles import finders
+
 
 # Create your views here.
 def index(request):
-    return render(request, "Narwhal_Tutoring/index.html")
+    tutors = User.objects.all()
+
+    for tutor in tutors:
+        username = tutor.username
+        pfp_url = f"{username}.png"
+        # Check if the image file exists
+        pfp_path = finders.find(f'images/{pfp_url}')
+        print(f"Generated path for {username}: {pfp_path}")
+        if pfp_path:
+            tutor.pfp_url = pfp_url
+        else:
+            # Set default pfp_url if the file doesn't exist
+            tutor.pfp_url = 'default.png'
+        tutor.save()
+
+
+    return render(request, "Narwhal_Tutoring/index.html", {
+        "tutors": tutors
+    })
 
 def login_view(request):
     if request.method == "POST":
@@ -21,7 +43,7 @@ def login_view(request):
         # Check if authentication successful
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse("index", kwargs={'page': 1}))
+            return HttpResponseRedirect(reverse("index"))
         else:
             return render(request, "Narwhal_Tutoring/login.html", {
                 "message": "Invalid username and/or password."
@@ -33,7 +55,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse("index", kwargs={'page': 1}))
+    return HttpResponseRedirect(reverse("index"))
 
 
 def register(request):
@@ -52,12 +74,23 @@ def register(request):
         # Attempt to create new user
         try:
             user = User.objects.create_user(username, email, password)
+
+            # Check if the image file exists before setting pfp_url
+            pfp_url = f"{username}.png"
+            # Check if the image file exists
+            pfp_path = finders.find(f'images/{pfp_url}')
+            print(f"Generated path for {username}: {pfp_path}")
+            if pfp_path:
+                user.pfp_url = pfp_url
+            else:
+                # Set default pfp_url if the file doesn't exist
+                user.pfp_url = 'default.png'
             user.save()
-        except IntegrityError:
+        except IntegrityError as e:
             return render(request, "Narwhal_Tutoring/register.html", {
-                "message": "Username already taken."
+                "message": e
             })
         login(request, user)
-        return HttpResponseRedirect(reverse("index", kwargs={'page': 1}))
+        return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "Narwhal_Tutoring/register.html")
